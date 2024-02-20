05efe4ad971ef11721fb6b2f3c82d6677d6411c1
marcofalvarez <mfalvarez@me.com>
2024-02-21 00:06:06 +0100

    formating media goles graph

--- pages/4 Equipos.py
+++ pages/4 Equipos.py
@@ -132,7 +132,7 @@ if make_table:
     # Añadir anotación
     fig_scatter.add_annotation(x=50, y=50, text="Línea de equilibrio", showarrow=False, font=dict(color="red"))
 
-    fig_scatter.update_traces(marker=dict(size=equipos['Goles_a_favor'], color='#ffcbcb', opacity=0.8),
+    fig_scatter.update_traces(marker=dict(size=equipos['Goles_a_favor'], color='#eaf6f6', opacity=0.8),
                              textfont=dict(color="#132743"),  # Cambiar el color del texto
                              textposition='top center')  # Colocar el texto encima del marcador
     
