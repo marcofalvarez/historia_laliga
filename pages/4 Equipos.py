f4cc0342f488b2bb125467983b9a296ab75aaaaa
marcofalvarez <mfalvarez@me.com>
2024-02-23 20:07:09 +0100

    update de equipos por JL

--- pages/4 Equipos.py
+++ pages/4 Equipos.py
@@ -193,21 +193,8 @@ if make_table:
     else:
         st.warning("No se encontró información del estadio para el equipo seleccionado.")
 
-
-
-
-    # Cargar los datos de los estadios
+# Cargar los datos de clasificacion
     clasificacion = pd.read_csv("data/clasificacion.csv")
-
-    # Agregar la parte fija a las URLs relativas
-    estadios_df['URL'] = 'https://es.wikipedia.org/' + estadios_df['URL']
-
-    # Obtener la URL del equipo seleccionado
-    url_equipo_seleccionado = estadios_df[estadios_df['Equipo'] == selec_team]['URL'].values[0]
-
-    # Mostrar el enlace encima de los gráficos de donut
-    st.markdown(f"[Más información sobre {selec_team} en Wikipedia]({url_equipo_seleccionado})")
-
 
     # Filtrar los datos para el equipo seleccionado desde la temporada 1987-88
     clasif_equipo_desde_1987 = clasificacion[(clasificacion['Equipo'] == selec_team) & (clasificacion['Temporada'] >= '1987-88')]
@@ -228,11 +215,6 @@ if make_table:
     # Calcular el número de veces que ha ganado la liga
     veces_ganadas = clasificacion[(clasificacion['Equipo'] == selec_team) & (clasificacion['Posicion'] == 1)].shape[0]
 
-    # Encontrar el récord de puesto de clasificación y el número de puntos conseguidos
-    #mejor_clasificacion = clasificacion[clasificacion['Equipo'] == selec_team]['Posicion'].min()
-    #año_mejor_clasificacion = clasificacion[(clasificacion['Equipo'] == selec_team) & (clasificacion['Posicion'] == mejor_clasificacion)]#['Temporada'].iloc[0]
-    #puntos_año_mejor_clasificacion = clasificacion[(clasificacion['Equipo'] == selec_team) & (clasificacion['Posicion'] == mejor_clasificacion)]#['PT'].iloc[0
-
     # Encontrar todos los récords de clasificación para el equipo seleccionado
     records_clasificacion = clasificacion[clasificacion['Equipo'] == selec_team].groupby(['Posicion', 'Temporada']).agg({'PT': 'max'}).reset_index()
 
@@ -241,59 +223,130 @@ if make_table:
     records_mejor_clasificacion = records_clasificacion[records_clasificacion['Posicion'] == mejor_clasificacion]
 
     # Mostrar las tarjetas
-    st.subheader("Estadísticas del equipo seleccionado:")
+    st.subheader(f"Estadísticas del {selec_team}")
     st.write("---")  # Agregar una línea horizontal para separar las estadísticas
 
     # Media de puntos por temporada (Histórica)
-    media_puntos_historica_str = f"Media de puntos por temporada (Histórica): {media_puntos_historica:.2f} puntos"
-    st.write(media_puntos_historica_str)
+
+    # Formatear la media de puntos por temporada histórica con dos decimales
+    media_puntos_historica_formateada = "{:.2f}".format(media_puntos_historica)
+
+    # Mostrar la métrica
+    st.metric(label="Media de puntos por temporada (Histórica):", value=media_puntos_historica_formateada, delta=None)
+
+    # Filtrar los datos para el equipo seleccionado
+    df_equipo = clasificacion[clasificacion['Equipo'] == selec_team]
+    df_equipo["Temporada"] = df_equipo['Temporada'].map(lambda x : x.split("-")[0])
+
+    # Crear el gráfico de puntos por temporada
+    fig = px.line(df_equipo, x='Temporada', y='PT', title=f'Puntos por temporada para {selec_team}')
+
+    # Agregar una línea para la media histórica de puntos
+    fig.add_trace(go.Scatter(x=df_equipo['Temporada'], y=[media_puntos_historica] * len(df_equipo),
+                             mode='lines', name='Media Histórica de Puntos', line=dict(color='red')))
+
+    # Especificar que el eje x debe mostrarse como entero
+    fig.update_xaxes(type='category')
+
+    # Mostrar el gráfico
+    st.plotly_chart(fig)
+
+    st.write("---")  # Agregar una línea horizontal para separar las estadísticas
 
     # Media de puntos por temporada (Desde 1987)
-    media_puntos_desde_1987_str = f"Media de puntos por temporada (Desde 1987): {media_puntos_desde_1987:.2f} puntos. (Este apartado lo hacemos para ver la media de puntos desde que hay 20 equipos)"
-    st.write(media_puntos_desde_1987_str)
+
+    # Formatear la media de puntos por temporada histórica con dos decimales
+    media_puntos_1987_formateada = "{:.2f}".format(media_puntos_desde_1987)
+
+    # Mostrar la métrica
+    st.metric(label="Media de puntos por temporada (Histórica):", value=media_puntos_1987_formateada, delta= "Este apartado lo hacemos para ver la media de puntos desde que hay 20 equipos")
+    
+    #media_puntos_desde_1987_str = f"Media de puntos por temporada (Desde 1987): {media_puntos_desde_1987:.2f} puntos. (Este apartado lo hacemos para ver la media de puntos desde que hay 20 equipos)"
+    #st.write(media_puntos_desde_1987_str)
+
+    # Filtrar los datos para el equipo seleccionado y desde 1987
+    df_equipo = clasificacion[(clasificacion['Equipo'] == selec_team) & (clasificacion['Temporada'].str[:4].astype(int) >= 1987)]
+    df_equipo["Temporada"] = df_equipo['Temporada'].map(lambda x: x.split("-")[0])
+
+    # Calcular la media de puntos desde 1987 para el equipo seleccionado
+    media_puntos_desde_1987 = df_equipo['PT'].mean()
+
+    # Crear el gráfico de puntos por temporada
+    fig = px.line(df_equipo, x='Temporada', y='PT', title=f'Puntos por temporada para {selec_team}')
+
+    # Agregar una línea para la media histórica de puntos desde 1987
+    fig.add_trace(go.Scatter(x=df_equipo['Temporada'], y=[media_puntos_desde_1987] * len(df_equipo),
+                             mode='lines', name='Media de Puntos desde 1987', line=dict(color='red')))
+
+    # Especificar que el eje x debe mostrarse como entero
+    fig.update_xaxes(type='category')
+
+    # Mostrar el gráfico
+    st.plotly_chart(fig)
+
+    st.write("---")  # Agregar una línea horizontal para separar las estadísticas
 
     # Número de veces jugadas
-    veces_jugadas_str = f"Número de temporadas jugadas: {temporadas_jugadas}"
-    st.write(veces_jugadas_str)
+    st.metric(label="Número de temporadas jugadas:", value = temporadas_jugadas, delta=None)
+
+    st.write("---")  # Agregar una línea horizontal para separar las estadísticas
 
     # Número de veces ganadas
-    veces_ganadas_str = f"Número de ligas ganadas: {veces_ganadas}"
-    st.write(veces_ganadas_str)
+    st.metric(label="Número de ligas ganadas:", value = veces_ganadas, delta=None)
+
+    # Verificar si hay ligas ganadas para mostrar el gráfico
+    if veces_ganadas > 0:
+
+        # Filtrar los datos para las temporadas en las que el equipo ganó la liga
+        temporadas_ganadas = records_mejor_clasificacion[records_mejor_clasificacion['Posicion'] == 1]
+
+        # Extraer el año de la temporada
+        temporadas_ganadas["Año"] = temporadas_ganadas['Temporada'].map(lambda x: x.split("-")[0])
+
+        # Crear una lista para almacenar los puntos y los equipos del segundo clasificado
+        puntos_segundo = []
+        equipos_segundo = []
+
+        # Iterar sobre las temporadas ganadas para obtener los puntos y el equipo del segundo clasificado
+        for temporada in temporadas_ganadas['Temporada']:
+            # Filtrar los datos para la temporada actual y encontrar el segundo clasificado
+            segundo_clasificado = clasificacion[(clasificacion['Temporada'] == temporada) & (clasificacion['Posicion'] == 2)]
+            # Agregar los puntos y el equipo del segundo clasificado a las listas
+            puntos_segundo.append(segundo_clasificado['PT'].iloc[0])
+            equipos_segundo.append(segundo_clasificado['Equipo'].iloc[0])
+
+        # Agregar columnas al DataFrame temporadas_ganadas para el segundo clasificado
+        temporadas_ganadas['Puntos_Segundo'] = puntos_segundo
+        temporadas_ganadas['Equipo_Segundo'] = equipos_segundo
+
+        # Crear el gráfico de barras apiladas
+        fig = go.Figure()
+
+        # Agregar las barras del equipo ganador y del segundo clasificado
+        fig.add_trace(go.Bar(x=temporadas_ganadas['Año'], y=temporadas_ganadas['PT'], name='Equipo Ganador', marker_color='blue'))
+        fig.add_trace(go.Bar(x=temporadas_ganadas['Año'], y=temporadas_ganadas['Puntos_Segundo'], name='Segundo Clasificado', marker_color='lightgreen'))
+
+        # Personalizar el diseño del gráfico
+        fig.update_layout(barmode='group', title=f'Años en los que {selec_team} ganó la liga y puntos obtenidos',
+                          xaxis_title='Temporada', yaxis_title='Puntos', showlegend=True, width = 1300)
+
+        # Mostrar el gráfico
+        st.plotly_chart(fig)
+
+    else:
+        st.write("No hay ligas ganadas para mostrar.")
+
+    st.write("---")  # Agregar una línea horizontal para separar las estadísticas
 
     # Récord de clasificación
     if len(records_mejor_clasificacion) > 1:
         st.write("Récord de clasificación:")
-        st.table(records_mejor_clasificacion[['Posicion', 'Temporada', 'PT']])
+        st.dataframe(records_mejor_clasificacion[['Posicion', 'Temporada', 'PT']])
     else:
         año_mejor_clasificacion = records_mejor_clasificacion.iloc[0]['Temporada']
         puntos_año_mejor_clasificacion = records_mejor_clasificacion.iloc[0]['PT']
-        st.write(f"Mejor puesto en la clasificación {mejor_clasificacion}º en la temporada{año_mejor_clasificacion} con un total de {puntos_año_mejor_clasificacion} pts")
+        # Mejor puesto en la clasificación
+        st.metric(label=f"Mejor puesto en la clasificación:", value=f"{mejor_clasificacion}º en la temporada {año_mejor_clasificacion} con {puntos_año_mejor_clasificacion} pts", delta= None)
 
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        #st.write(f"Mejor puesto en la clasificación {mejor_clasificacion}º en la temporada{año_mejor_clasificacion} con un total de {puntos_año_mejor_clasificacion} pts")
 
