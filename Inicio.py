6f4e89df1cd8f00ab48e5d34ede9406b8f758ffc
marcofalvarez <mfalvarez@me.com>
2024-02-24 21:09:35 +0100

    update text Inicio justify

--- Inicio.py
+++ Inicio.py
@@ -12,9 +12,9 @@ st.title('Historia de La Liga Española')
 st.divider()
 st.markdown(
     '''
-        Esta aplicación está diseñada para obtener el máximo de información sobre  
-        los resultados históricos de La Liga. En el menú de la izquierda podrás   
-        aprender al interactuar con representaciones visuales de los datos desde 1928.  
+        Esta aplicación está diseñada para obtener el máximo de información sobre los 
+        resultados históricos de La Liga. En el menú de la izquierda podrás aprender al 
+        interactuar con representaciones visuales de los datos desde 1928.  
     '''
 )
 st.divider()  
@@ -22,22 +22,22 @@ st.markdown(
     '''    
          ### Hemos dividido la información en 4 páginas:
             1.Resumen:     
-                        Contiene información general de los equipos con más participaciones  
-                        más participaciones en la liga (más de 20). Podrás explorar sus   
-                        úbicaciones geográficas el número de trofeos de La Liga y cuándo los  
-                        obtuvieron.   
+                        Contiene información general de los equipos con más participaciones 
+                        más participaciones en la liga (más de 20). Podrás explorar sus 
+                        úbicaciones geográficas el número de trofeos de La Liga y cuándo 
+                        los obtuvieron.   
             2.Hisóricos:   
-                        Con estádisticas historicas para los equipos que quieras ver. Podrás  
-                        hacer comparaciones de equipos a través de los años y enfocarte en  
-                        los periodos de tiempo y los datos que más te interesen.   
+                        Con estádisticas historicas para los equipos que quieras ver. Podrás 
+                        hacer comparaciones de equipos a través de los años y enfocarte en los 
+                        periodos de tiempo y los datos que más te interesen.   
                            
             3.Temporadas:   
-                        Esta página esta diseñada para date un resumen de lo que ha pasado  
-                        cada temporada de La Liga. Podrás escoger el año que te interese y  
-                        descubrir los detalles más interesantes.   
+                        Esta página esta diseñada para date un resumen de lo que ha pasado cada 
+                        temporada de La Liga. Podrás escoger el año que te interese y descubrir 
+                        los detalles más interesantes.   
             4.Equipos:   
-                        Y para conocer más sobre tu equipo favorito o aprender un poco sobre  
-                        algún equipo rival podrás visitar esta página.   
+                        Y para conocer más sobre tu equipo favorito o aprender un poco sobre algún 
+                        equipo rival podrás visitar esta página.   
      '''                       
     
 )
