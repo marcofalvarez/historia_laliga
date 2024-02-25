c46e89ed25c3a3d21f98b0da51d7563c28d150ac
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-02-25 18:45:52 +0100

    Formatear correctamente las viñetas

--- Inicio.py
+++ Inicio.py
@@ -20,21 +20,21 @@ st.divider()
 st.markdown(
     '''    
          ### Hemos dividido la información en 4 páginas:
-            1.Resumen:     
+            1. Resumen:     
                         Contiene información general de los equipos con más participaciones  
                         más participaciones en la liga (más de 20). Podrás explorar sus   
                         úbicaciones geográficas el número de trofeos de La Liga y cuándo los  
                         obtuvieron.   
-            2.Hisóricos:   
-                        Con estádisticas historicas para los equipos que quieras ver. Podrás  
+            2. Históricos:   
+                        Con estadísticas históricas para los equipos que quieras ver. Podrás  
                         hacer comparaciones de equipos a través de los años y enfocarte en  
                         los periodos de tiempo y los datos que más te interesen.   
                            
-            3.Temporadas:   
+            3. Temporadas:   
                         Esta página esta diseñada para date un resumen de lo que ha pasado  
                         cada temporada de La Liga. Podrás escoger el año que te interese y  
                         descubrir los detalles más interesantes.   
-            4.Equipos:   
+            4. Equipos:   
                         Y para conocer más sobre tu equipo favorito o aprender un poco sobre  
                         algún equipo rival podrás visitar esta página.   
      '''                       
